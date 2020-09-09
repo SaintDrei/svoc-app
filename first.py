a2861d5744865e53d7f9f72f1a95d30e7cd58298
Andrei Santos <andreimishael.santos@benilde.edu.ph>
2020-09-09 21:46:13 +0800

    Update 009
    UPdating write row function

--- first.py
+++ first.py
@@ -30,6 +30,7 @@ def prep_csv():
 
 table_prepped = prep_csv()
 table_transposed = table_prepped.T
+table_OUT = table_prepped
 
 def count_clusters(x):
     counts = x["PIVOT"].value_counts()
@@ -40,15 +41,31 @@ def count_matches(x):
     y = x["MATCH_ID"].value_counts()
     return y.sum(axis=0)
 
+# def update_row(x, y, z):
+
 
 def showview(x):
     st.write(st.table(x))
-    remark = st.text_input("Remarks", )
+   # showButtons()
+
+def showButtons():
+    #remarks = st.text_input("Remarks", )
     timeApproved = datetime.now()
     st.write(timeApproved)
-    st.button("Approve") 
-    st.button("Reject")
-
+    if st.button("Approve"):
+        return "Approve"
+    if st.button("Reject"):
+        return "Reject"
+    
+def writeRow(row, approval, prev, table_OUT):
+    row.loc['STEWARD_APPROVAL'] = approval
+    row.loc['DATE_APPROVED'] = datetime.now()
+    row.loc['STEWARD'] = stewardName
+    row.loc['TIME_TAKEN'] = datetime.now() - prev
+    st.write(table_OUT.loc[table_OUT['RECORD_ID'] == row.RECORD_ID])
+    table_OUT.loc[table_OUT['RECORD_ID'] == row.RECORD_ID] = row
+    st.write('test', row)
+    
 
 st.write(count_matches(table_prepped))
 
@@ -67,12 +84,21 @@ for index, row in table_pivots.iterrows():
     for index, row in table_comp.iterrows():
         st.write("match", index)
         outable = pd.concat([pasdt, row], axis=1)
-        
+        st.write(row.loc['RECORD_ID'])
         st.write("Compare the values and press Reject or Approve accordingly")
-
+        prev = datetime.now()
         showview(outable)
-        st.table(pd.concat([pasdt, row], axis=1))
-    
+        writeRow(row, showButtons(), prev, table_OUT)
+        st.write("End of match", index)
+        #if showview(outable) == "1":
+        #    continue
+        #return next(row)
+    else:
+        break
+
+else: 
+    st.write("Finished")
+
 st.write("end test")
 st.table(table_pivots.T)
 #st.write(table_prepped[table_prepped>=1].sum(axis=4))    
@@ -98,7 +124,8 @@ try:
 except:
     st.write("ahh")
 
-
+st.write("Table OUT")
+st.table(table_OUT)
 #print(pd.DataFrame.equals(table_CSV))
 
 # Sidebar - END
